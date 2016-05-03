9309c7d5a3888bd17863367ce172e1d1d6b385e6
Susanna Riccardi <SusannaChuck@MacBook-Pro-di-Susanna.local>
2016-05-03 15:00:32 +0200

    Added map on locationTab.

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
@@ -7,14 +7,85 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by patrickbalestra on 14/03/2015.
- */
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class LocationTab extends Fragment {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflat and return the layout.
         View view = inflater.inflate(R.layout.location_tab, container, false);
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        // Display the map immediately.
+        mapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mapView.getMap();
+        // TODO: Get location. Used a predefined location for now.
+        double latitude = 46.011198;
+        double longitude = 8.957746;
+
+        // Marker on the map.
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("You are here");
+
+        // Marker icon options.
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+        // Adding marker on the map.
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                // TODO: Implement when get location is done.
+                .target(new LatLng(46.011198, 8.957746)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here.
         return view;
     }
+
+    // Map callbacks.
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
+
