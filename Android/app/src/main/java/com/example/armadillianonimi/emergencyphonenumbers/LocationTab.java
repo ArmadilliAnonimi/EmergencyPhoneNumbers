7114800c61d316574273e6a6e40699fa35eb409e
Susanna Riccardi <susi.96@hotmail.it>
2016-05-03 18:40:30 +0200

    If this commit works, added get current location.

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
@@ -1,7 +1,14 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +18,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -20,6 +28,29 @@ public class LocationTab extends Fragment {
 
     private MapView mapView;
     private GoogleMap googleMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            System.out.println("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            System.out.println("Latitude"+"disable");
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            System.out.println("Latitude"+"enable");
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            System.out.println("Latitude"+"status");
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,6 +61,23 @@ public class LocationTab extends Fragment {
 
         // Display the map immediately.
         mapView.onResume();
+
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            System.out.println("We don't have permissions.");
+//            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        System.out.println("We have permissions.");
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -54,7 +102,7 @@ public class LocationTab extends Fragment {
         googleMap.addMarker(marker);
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 // TODO: Implement when get location is done.
-                .target(new LatLng(46.011198, 8.957746)).zoom(12).build();
+                .target(new LatLng(76.011198, 8.957746)).zoom(12).build();
         googleMap.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
 
@@ -88,4 +136,6 @@ public class LocationTab extends Fragment {
     }
 
 }
+
+
 
