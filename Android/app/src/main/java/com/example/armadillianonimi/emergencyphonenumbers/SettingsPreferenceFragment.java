c024c3202cf6b1c0d9614e061068aa3b9dfa9d71
falcon <aron.fiechter@gmail.com>
2016-05-05 19:56:54 +0200

    Settings Tab is working now. Added two placeholder switches.

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsPreferenceFragment.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsPreferenceFragment.java
@@ -1,18 +1,37 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
  * Created by Aron Fiechter on 2016-05-04.
  */
-public class SettingsPreferenceFragment extends PreferenceFragment {
+public class SettingsPreferenceFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
+    public void onCreatePreferences(Bundle bundle, String s) {
+        System.out.println("SettingsPreferenceFragment has been opened.");
         addPreferencesFromResource(R.xml.preferences);
         }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
     }
--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
@@ -1,8 +1,8 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +15,30 @@ public class SettingsTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_tab, container, false);
+        System.out.println("SettingsTab has been opened.");
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsPreferenceFragment()).commit();
+        getFragmentManager().beginTransaction().replace(R.id.settings_container_tab, new SettingsPreferenceFragment()).commit();
+        System.out.println("Replaced view. Returning...");
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
     }
 }
