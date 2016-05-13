32b35845ee571e21106bc15769e4421f6782159b
falcon <aron.fiechter@gmail.com>
2016-05-13 03:17:54 +0200

    Removed view substitution in SettingsTab, which is now directly a PreferenceFragmentCompat.

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
@@ -1,25 +1,16 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
  * Created by Aron Fiechter on 2016-05-04.
  */
-public class SettingsTab extends Fragment {
+public class SettingsTab extends PreferenceFragmentCompat {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.settings_tab, container, false);
-        System.out.println("SettingsTab has been opened.");
-
-        getFragmentManager().beginTransaction().replace(R.id.settings_container_tab, new SettingsPreferenceFragment()).commit();
-        System.out.println("Replaced view. Returning...");
-        return view;
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override
