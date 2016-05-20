b0118f05d009df8bbd6c13502c29d4a3f7a7588c
Susanna Riccardi <susi.96@hotmail.it>
2016-05-20 17:02:18 +0200

    Refactor location code and implemented the magical button for the location. Now it works!

---
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationManagerListener.java
@@ -0,0 +1,5 @@
+package com.example.armadillianonimi.emergencyphonenumbers;
+
+public interface LocationManagerListener {
+    void locationReceived(UserLocation location);
+}
--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/LocationTab.java
@@ -1,22 +1,15 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -26,17 +19,16 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class LocationTab extends Fragment {
 
+    private LocationFinder locationFinder;
     private MapView mapView;
     private TextView addressTextView;
     private GoogleMap googleMap;
-    private LocationManager locationManager;
     private Marker marker;
     private LocationListener locationListener = new LocationListener() {
         @Override
@@ -96,7 +88,7 @@ public class LocationTab extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflat and return the layout.
+        // Inflate and return the layout.
         View view = inflater.inflate(R.layout.location_tab, container, false);
         mapView = (MapView) view.findViewById(R.id.mapView);
         addressTextView = (TextView) view.findViewById(R.id.addresstextview);
@@ -104,29 +96,52 @@ public class LocationTab extends Fragment {
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
 
-        // Initialize the location manager
-        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationFinder = new LocationFinder(getActivity());
+        locationFinder.setLocationManagerListener(new LocationManagerListener() {
+            @Override
+            public void locationReceived(UserLocation location) {
+                addressTextView.setText(location.completeAddress);
+
+                googleMap = mapView.getMap();
+
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                // Adding marker on the map.
+                marker = googleMap.addMarker(new MarkerOptions().position(location.position).title("You are here").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                // Moving position of the camera in the map depending on the position of the marker.
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(location.position).zoom(16).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
         return view;
     }
 
+    // If we have permissions to access location, load the map.
     @Override
     public void onStart() {
         super.onStart();
-        requestPermissionToAccessLocation();
+        if (locationFinder.hasPermissionToAccessLocation()) {
+            loadMap();
+        }
     }
 
+    // Method that creates the map.
     public void loadMap() {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationFinder.requestLocation();
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    // If the provider is disabled, set the addressTextView as:
+    public void setProviderDisabled() {
+        addressTextView.setText("Location disabled.\nPlease check your settings.");
     }
 
     // Map callbacks.
@@ -140,9 +155,6 @@ public class LocationTab extends Fragment {
     public void onPause() {
         super.onPause();
         mapView.onPause();
-        if (locationManager != null && checkLocationPermission()) {
-            locationManager.removeUpdates(locationListener);
-        }
     }
 
     @Override
@@ -155,36 +167,5 @@ public class LocationTab extends Fragment {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
-    }
-
-    private void requestPermissionToAccessLocation() {
-        // If the access to the location was not yet granted, ask for it.
-        if (!checkLocationPermission()) {
-            System.out.println("Requesting permission");
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-        } else {
-            System.out.println("load map");
-            loadMap();
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    loadMap();
-                }
-            }
-            // Check other permissions in the future
-        }
-    }
-
-    public boolean checkLocationPermission() {
-        String permission = "android.permission.ACCESS_FINE_LOCATION";
-        String permissionCoarse = "android.permission.ACCESS_COARSE_LOCATION";
-        int res = getActivity().checkCallingOrSelfPermission(permission);
-        int resCoarse = getActivity().checkCallingOrSelfPermission(permissionCoarse);
-        return res == PackageManager.PERMISSION_GRANTED && resCoarse == PackageManager.PERMISSION_GRANTED;
     }
 }
