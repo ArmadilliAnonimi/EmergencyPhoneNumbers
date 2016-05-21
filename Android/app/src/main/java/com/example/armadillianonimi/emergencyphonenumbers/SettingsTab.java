3f9726b8699deda6cd17cfc5d4bf1cd83fc1bca7
falcon <aron.fiechter@gmail.com>
2016-05-21 06:25:17 +0200

    Country selection link from settings, added strings for disclaimer.

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/SettingsTab.java
@@ -1,7 +1,12 @@
 package com.example.armadillianonimi.emergencyphonenumbers;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.preference.DialogPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.AttributeSet;
 
 /**
  * Created by Aron Fiechter on 2016-05-04.
@@ -11,6 +16,17 @@ public class SettingsTab extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        // Listener for click on "Change country" preference
+        Preference dialogPref = findPreference("dialog");
+        dialogPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                android.app.FragmentManager fm = getActivity().getFragmentManager();
+                CountrySelectionDialog Country_Selector = new CountrySelectionDialog();
+                Country_Selector.show(fm, "Country_Selector");
+                return true;
+            }
+        });
     }
 
     @Override
