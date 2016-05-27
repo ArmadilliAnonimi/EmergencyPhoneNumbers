5b0272698712ebe637cdf8573d1be0738b85b33a
Patrick Balestra <me@patrickbalestra.com>
2016-05-28 00:20:24 +0200

    Add FAQ text

--- Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/DisclaimerActivity.java
+++ Android/app/src/main/java/com/example/armadillianonimi/emergencyphonenumbers/DisclaimerActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class DisclaimerActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclaimer);
         Toolbar disclaimerToolbar = (Toolbar) findViewById(R.id.disclaimer_toolbar);
+        disclaimerToolbar.setTitle("FAQ");
         setSupportActionBar(disclaimerToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Window window = getWindow();
@@ -24,6 +28,9 @@ public class DisclaimerActivity extends AppCompatActivity {
                  finish();
             }
         });
+
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(R.string.faq);
 
     }
 }
